b843524a96e5cb0e0acf42fbd39211e4ae8eeb6f
Aleksander <alek.mlynarski05@gmail.com>
2025-04-14 00:34:40 +0200

    Postępy dnia 1.

--- biegacz.py
+++ biegacz.py
@@ -1,12 +1,28 @@
 import pygame
+import os
+import math
+os.chdir(os.path.expanduser("~/Documents/gra"))
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
+screen = pygame.display.set_mode((576,324))
 pygame.display.set_caption('Biegacz')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Rainbow Memories.otf', 50)
 
-test_surface = pygame.Surface((120,56))
+tlo = pygame.image.load('graphics/background/1 background/orig.png')
+bohater1 = pygame.image.load('graphics/player/1 Pink_Monster/Pink_Monster.png')
+text_surface = test_font.render('Start', True, 'White')
+crystal1 = pygame.image.load('graphics/objects/4.png')
+
+bohater1 = pygame.transform.scale(bohater1, (64, 64))
+crystal1 = pygame.transform.scale(crystal1, (64, 64))
+crystal1_x_pos = 0
+crystal1_y_pos = 0
+center_x = 250
+center_y = 115
+radius = 100
+angle = 0
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +30,12 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface,(0,0))
+    screen.blit(tlo,(0,0))
+    screen.blit(crystal1,(crystal1_x_pos,crystal1_y_pos))
+    crystal1_x_pos = center_x + radius * math.cos(angle)
+    crystal1_y_pos = center_y + radius * math.sin(angle)
+    angle += 0.03
+    screen.blit(text_surface,(40,15))
 
     pygame.display.update()
     clock.tick(60)
